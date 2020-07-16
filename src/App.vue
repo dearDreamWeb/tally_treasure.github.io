e3781dda418d7bb39a2647ec130925b8668396b1
wxb <m18837120047@163.com>
2020-07-16 16:53:12 +0800

    v0.9 修改了logo，pc端提示二维码

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,30 @@
 <script>
 export default {
   methods: {
+    // 判断设备是移动端还是PC端
     device() {
       if (
-        navigator.userAgent.match(
+        !navigator.userAgent.match(
           /(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i
         )
-      ) {
-        console.log("手机访问");
-      } else {
-        console.log("电脑访问");
-      }
+      )
+        this.$alert(
+          <div style="text-align:center; font-size:20px">
+            <h1 style="margin-bottom:10px">
+              请手机扫描二维码或者使用浏览器手机模式浏览
+            </h1>
+            <div>
+              <img
+                src="https://s1.ax1x.com/2020/07/16/UDwCE8.png"
+                alt="二维码"
+              />
+            </div>
+          </div>,
+          "提示",
+          {
+            dangerouslyUseHTMLString: true,
+          }
+        );
     },
   },
   mounted() {
